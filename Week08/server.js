659c9161c2d3136619fd7e39d0d5e86f3a874db0
lijing <1609071695@qq.com>
2021-01-27 02:58:16 +0800

    状态机|http请求

---
+++ Week08/server.js
@@ -0,0 +1,17 @@
+const http = require("http")
+
+http.createServer((req, res) => {
+    let body = [];
+    req.on('error', (err) => {
+        console.error('err',err);
+    }).on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log('body',body);
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.end('test')
+    });
+}).listen('8088')
+
+console.log("server is start!")
